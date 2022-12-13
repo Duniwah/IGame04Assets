359a342bf8da068e0d64187960cec44707ad1ecc
dengrh <dengrh@163.com>
2022-12-13 18:45:52 +0800

    18.45

--- minigame/clundfunctions/Rank/index.js
+++ minigame/clundfunctions/Rank/index.js
@@ -4,6 +4,8 @@ const cloud = require('wx-server-sdk');
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 const db = cloud.database();
 const collection = db.collection("IGame04_Rank");
+const MAX_LIMIT = 100;
+
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -19,23 +21,60 @@ exports.main = async (event, context) => {
 }
 
 const uploadScore = async (_openid, userInfo) => {
-  const id = userInfo._id;
-  delete userInfo._id;
-  userInfo._serverDate = db.serverDate();
+  let info = {
+    wxgame: {
+      score: userInfo.wxgame.score,
+      update_time: userInfo.wxgame.update_time,
+    },
+    coin: userInfo.coin,
+    // name:
+    // url
+    _serverDate = db.serverDate()
+  }
+  let msg;
   await collection.doc(_openid).set({
-    data: userInfo
-  });
-  userInfo._id = id;
-  return userInfo;
+    data: info
+  }).then(res => {
+    msg = res
+  })
+  return {
+    data: info,
+    msg = msg,
+  }
 }
 
 const getScoreRankInfo = async (_openid, data) => {
-  const all_data = await collection.orderBy('score', 'desc')
-    .skip(data.offset)
-    //偏移量
-    .limit(data.count)
-    //数量
-    .get();
-  const all_data_data = all_data.data;
-  return all_data_data;
+  // //orderBy 排序
+  // const all_data = await collection.orderBy('wxgame.score', 'desc')
+  //   //偏移量
+  //   .skip(data.offset)
+  //   //数量
+  //   .limit(data.count)
+  //   .get();
+  // const all_data_data = all_data.data;
+  // return all_data_data;
+  let count = data.count;
+  let offset = data.offset;
+  let batchTimes = Math.ceil(count / MAX_LIMIT);
+  let tasks = [];
+  for (let i = 0; i < batchTimes; i++) {
+    let skipCount = i * MAX_LIMIT;
+    let limitCount;
+    if (count - skipCount > MAX_LIMIT) {
+      limitCount = MAX_LIMIT;
+    } else {
+      limitCount = count - skipCount;
+    }
+    let promis = collection.orderBy('wxgame.score', 'desc')
+      .skip(skipCount + offset)
+      .limit(limitCount)
+      .get();
+    tasks.push(promis);
+  }
+  return (await Promise.all(tasks)).reduce((acc, cur) => {
+    return {
+      data: acc.data.concat(cur.data),
+      errMsg: acc.errMsg,
+    }
+  })
 }
--- minigame/clundfunctions/UserData/index.js
+++ minigame/clundfunctions/UserData/index.js
@@ -1,6 +1,14 @@
 
 // 云函数入口函数
 exports.main = async (event, context) => {
+  let { OPENID, APPID, UNIONID } = cloud.getWXContext();
+  let { func, data } = event;
+  let res;
+  if (func === 'reportData') {
+    res = await reportData();
+  } else if (func === 'getData') {
+
+  }
 
 }
 
